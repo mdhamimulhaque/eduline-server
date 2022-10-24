f8906e28a578ecbeaca345bf2334168558c7ae3a
Md Hamimul Haque <mdhamimulhaque888@gmail.com>
2022-10-25 01:09:57 +0600

    courser api done

--- index.js
+++ index.js
@@ -4,9 +4,18 @@ var cors = require('cors')
 const port = 5000;
 
 app.use(cors())
+
+const courses = require("./data/courses.json");
+
+
 // ---> check server
 app.get('/', (req, res) => {
     res.send('server is running...')
+})
+
+// --->all courses 
+app.get('/courses', (req, res) => {
+    res.send(courses)
 })
 
 app.listen(port, () => {
